ba0dda8971dd294d9fffcb509b8064683a69e333
mantiguahub <mantigua@montgomerycollege.edu>
2025-07-07 21:41:59 -0400

    Update task 1.java

--- task 1.java
+++ task 1.java
@@ -1,3 +1,5 @@
+//Name: Michael Antigua
+//Cmsc 203
 public void printArray() {
     for (int row = 0; row < intArray.length; row++) {
         for (int column = 0; column < intArray[0].length; column++) {
