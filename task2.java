889fa22ef054dbfeafca7a165e709ff977411996
mantiguahub <mantigua@montgomerycollege.edu>
2025-07-07 21:43:17 -0400

    Update task2.java

--- task2.java
+++ task2.java
@@ -1,3 +1,5 @@
+//Name:Michael Antigua
+//CMSC 203
 public void setValues(int value, int row) {
     for (int column = 0; column < intArray[0].length; column++) {
         intArray[row][column] = value;
